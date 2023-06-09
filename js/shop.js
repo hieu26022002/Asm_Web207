9d6286a454014fc91de19ef09eaff3834f591e77
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-06-09 21:29:04 +0700

    Update shop

--- js/shop.js
+++ js/shop.js
@@ -1,18 +1,10 @@
-const listProductsController = ($scope, $http) => {
-    $scope.productsList = [];
-
-    $http.get(API + "/products?_limit=8").then((res) => {
-        $scope.productsList = res.data
-    }).catch(() => {
-        alert("Đã có lỗi xảy ra!!!")
-    })
-}
 
 const paginateController = function ($scope, $http) {
     $scope.currentPage = 1; // Trang hiện tại
     $scope.itemsPerPage = 8; // Số sản phẩm hiển thị trên mỗi trang
     $scope.totalItems = 0; // Tổng số sản phẩm
     $scope.products = []; // Danh sách sản phẩm trên trang hiện tại
+    $scope.pagination = [];
 
     // Lấy danh sách sản phẩm từ JSON Server
     $scope.getProducts = function () {
@@ -21,6 +13,8 @@ const paginateController = function ($scope, $http) {
             .then(function (response) {
                 $scope.products = response.data;
                 $scope.totalItems = parseInt(response.headers('X-Total-Count'));
+                $scope.totalPages = Math.ceil(response.headers('X-Total-Count') / 8);
+                generatePagination();
             })
             .catch(function (error) {
                 console.log('Lỗi khi lấy danh sách sản phẩm:', error);
@@ -28,10 +22,42 @@ const paginateController = function ($scope, $http) {
     };
 
     // Chuyển đến trang mới
-    $scope.goToPage = function (pageNumber) {
-        $scope.currentPage = pageNumber;
-        $scope.getProducts();
+    $scope.goToPage = function (page) {
+        if (page < 1 || page > $scope.totalPages) {
+            return;
+        }
+        console.log("Chuyển trang");
+        $scope.currentPage = page;
+        $scope.getProducts(page);
     };
+    // Khởi tạo
+    function generatePagination() {
+        $scope.pagination = [];
+        var startPage, endPage;
+        var totalVisiblePages = 4; // Số trang cụ thể hiển thị
+        if ($scope.totalPages <= totalVisiblePages) {
+            startPage = 1;
+            endPage = $scope.totalPages;
+        } else {
+            var currentPage = $scope.currentPage;
+            var halfVisiblePages = Math.floor(totalVisiblePages / 2);
+
+            if (currentPage <= halfVisiblePages) {
+                startPage = 1;
+                endPage = totalVisiblePages;
+            } else if (currentPage + halfVisiblePages >= $scope.totalPages) {
+                startPage = $scope.totalPages - totalVisiblePages + 1;
+                endPage = $scope.totalPages;
+            } else {
+                startPage = currentPage - halfVisiblePages;
+                endPage = currentPage + halfVisiblePages;
+            }
+        }
+
+        for (var i = startPage; i <= endPage; i++) {
+            $scope.pagination.push(i);
+        }
+    }
     // Khởi tạo
     $scope.getProducts();
 }
--- route.js
+++ route.js
@@ -1,6 +1,6 @@
 const clientApp = angular.module("clientApp", ["ngRoute"]);
 
-clientApp.controller("paginateController",paginateController)
+clientApp.controller("paginateController", paginateController)
 clientApp.controller("listBestSellerController", listBestSellerController)
 // Route client App
 clientApp.config(($routeProvider, $locationProvider) => {
@@ -10,18 +10,18 @@ clientApp.config(($routeProvider, $locationProvider) => {
         templateUrl: './pages/Client/home.html',
         controller: "listBestSellerController"
     })
-    .when("/products", {
-        templateUrl: './pages/Client/shop.html',
-        controller: "paginateController"
-    }).when("/product/:id", {
-        templateUrl: './pages/Client/detail.html'
-    }).when("/blogs", {
-        templateUrl: './pages/Client/blogs.html'
-    }).when("/checkout", {
-        templateUrl: './pages/Client/checkout.html',
-    }).otherwise({
-        redirectTo: "/"
-    })
+        .when("/products", {
+            templateUrl: './pages/Client/shop.html',
+            controller: "paginateController"
+        }).when("/product/:id", {
+            templateUrl: './pages/Client/detail.html'
+        }).when("/blogs", {
+            templateUrl: './pages/Client/blogs.html'
+        }).when("/checkout", {
+            templateUrl: './pages/Client/checkout.html',
+        }).otherwise({
+            redirectTo: "/"
+        })
 
 })
 
