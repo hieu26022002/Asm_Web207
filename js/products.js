253ef0aba5822087b6db5804acb4f53cb07c0444
NqDyK2 <nguyenquyduyabc@gmail.com>
2023-06-09 10:49:49 +0700

    Feat: Read products and delete

--- js/products.js
+++ js/products.js
@@ -1,31 +1,13 @@
-const listProducts = function ($scope, $http) {
-    $scope.productsList = [];
-
-    $http.get(API + "/products").then((res) => {
-        $scope.productsList = res.data
-    }).catch(() => {
-        alert("Đã có lỗi xảy ra!!!!")
-    })
-}
-
-function viewDetail($scope, $http, $routeParams, $location) {
-    $scope.id = $routeParams.id
-    $scope.item = {};
-    $http.get(API + "/products" + `/${$scope.id}`).then(function (res) {
-        $scope.item = res.data
-    }).catch(() => {
-        alert("Không thành công!!!")
-    })
-}
-
 function addController($scope, $http, $location) {
     $scope.account = {
         id: 0,
         name: "",
-        email: "",
-        password: "",
-        phone: "",
-        address: ""
+        price: 0,
+        img: "",
+        overview: "",
+        description: "",
+        address: "",
+        category_id: 0,
     }
 
     $scope.onSubmit = function (event) {
@@ -37,5 +19,101 @@ function addController($scope, $http, $location) {
             .catch(function (err) {
                 console.log(err);
             })
+    }
+}
+
+const listProducts = function ($scope, $http) {
+    $scope.currentPage = 1; // Trang hiện tại
+    $scope.itemsPerPage = 9; // Số sản phẩm hiển thị trên mỗi trang
+    $scope.totalItems = 0; // Tổng số sản phẩm
+    $scope.products = []; // Danh sách sản phẩm trên trang hiện tại
+    $scope.pagination = [];
+
+    $scope.getProducts = function (page) {
+        var url = API + `/products?_page=` + page + `&_limit=` + $scope.itemsPerPage;
+        $http.get(url)
+            .then(function (response) {
+                $scope.products = response.data;
+                $scope.totalItems = parseInt(response.headers('X-Total-Count'));
+                $scope.totalPages = Math.ceil(response.headers('X-Total-Count') / 9);
+                generatePagination();
+            })
+            .catch(function (error) {
+                console.log('Lỗi khi lấy danh sách sản phẩm:', error);
+            });
+    };
+    $scope.goToPage = function (page) {
+        if (page < 1 || page > $scope.totalPages) {
+            return;
+        }
+        console.log("Chuyển trang");
+        $scope.currentPage = page;
+        $scope.getProducts(page);
+    };
+
+    function generatePagination() {
+        $scope.pagination = [];
+        var startPage, endPage;
+        var totalVisiblePages = 4; // Số trang cụ thể hiển thị
+        if ($scope.totalPages <= totalVisiblePages) {
+            startPage = 1;
+            endPage = $scope.totalPages;
+        } else {
+            var currentPage = $scope.currentPage;
+            var halfVisiblePages = Math.floor(totalVisiblePages / 2);
+
+            if (currentPage <= halfVisiblePages) {
+                startPage = 1;
+                endPage = totalVisiblePages;
+            } else if (currentPage + halfVisiblePages >= $scope.totalPages) {
+                startPage = $scope.totalPages - totalVisiblePages + 1;
+                endPage = $scope.totalPages;
+            } else {
+                startPage = currentPage - halfVisiblePages;
+                endPage = currentPage + halfVisiblePages;
+            }
+        }
+
+        for (var i = startPage; i <= endPage; i++) {
+            $scope.pagination.push(i);
+        }
+    }
+    // Khởi tạo
+    $scope.getProducts();
+}
+
+function editController($scope, $http, $routeParams) {
+    const id = $routeParams.id;
+    $scope.product = {
+        id: 0,
+        name: "",
+        price: 0,
+        img: "",
+        overview: "",
+        description: "",
+        address: "",
+        category_id: 0,
+    }
+    $http.get(API + '/products/' + id).then(function (res) {
+        $scope.product = res.product
+    })
+
+    $scope.accept = function () {
+        $http.put(API + "/products/" + id, $scope.product).then(function () {
+            alert("Sửa thành công");
+        })
+    }
+}
+
+function deleteController($scope, $http, $routeParams, $location) {
+    $scope.id = $routeParams.id
+    if (confirm("Bạn có chắc chắn muốn xoá sản phẩm này?")) {
+        $http.delete(API + "/products" + `/${$scope.id}`).then(() => {
+            $location.path("/products")
+        }).catch(() => {
+            alert("Xoá không thành công!!!")
+        })
+    } else {
+        $location.path("/products")
     }
 }
